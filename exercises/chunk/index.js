ccf65d9031e190de467a253e351058c00a796edc
funduleadiana <diana.fundulea@yahoo.com>
2021-08-13 21:17:07 +0100

    steps to solve chuncked exercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,17 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  const chuncked = [];
-  for (let element of array){
-    const last = chuncked[chuncked.length - 1]
-    if(!last || last.length === size){
-      chuncked.push([element])
-    }else {
-      last.push(element)
-    }
-  }
-  return chuncked
-}
+// function chunk(array, size) {
+//   const chuncked = [];
+//   for (let element of array){
+//     const last = chuncked[chuncked.length - 1]
+//     if(!last || last.length === size){
+//       chuncked.push([element])
+//     }else {
+//       last.push(element)
+//     }
+//   }
+//   return chuncked
+// }
 
+
+function chunk(array, size) {
+  // create empty chuncked array
+  const chuncked =[];
+  //create index start at 0
+
+  //while index is less than array.length
+
+  //push a slice of length 'size' from 'array' into 'chuncked'
+
+  //add 'size' to 'index'
+}
 module.exports = chunk;
