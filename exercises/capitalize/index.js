338484fb210ee06ab47df492ba7270a9241d1abf
funduleadiana <diana.fundulea@yahoo.com>
2021-08-15 19:10:26 +0100

    capitalize first letter of every word in a string solution 2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,20 +8,38 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  //make an empty array
-  const words = [];
-  //split the input string by spaces
-  
-  //for each word in the array 
-for (let word of str.split(' ')){
-  //uppercase the first letter of each word
-  //join first letter with rest of the string
-  words.push(word[0].toUpperCase() + word.slice(1))
-}
-//push result into words array
-//join 'words into a string and return
- return words.join(' ') 
+  //create an empty string called result// the only content will be immediately capitalized
+  let result = str[0].toUpperCase();
+  //iterate through every character of the string
+  for (let i= 1; i<= str.length; i++){
+  //if the character to the left is a space
+  if(str[i-1] === ' '){
+    //Capitalize it and add it to result
+    result += str[i].toUpperCase()
+  }else{
+  //else - add it to 'result
+  result += str[i];
+  }}
 
+  return result;
 }
 
 module.exports = capitalize;
+
+
+// function capitalize(str) {
+//   //make an empty array
+//   const words = [];
+//   //split the input string by spaces
+  
+//   //for each word in the array 
+// for (let word of str.split(' ')){
+//   //uppercase the first letter of each word
+//   //join first letter with rest of the string
+//   words.push(word[0].toUpperCase() + word.slice(1))
+// }
+// //push result into words array
+// //join 'words into a string and return
+//  return words.join(' ') 
+
+// }
