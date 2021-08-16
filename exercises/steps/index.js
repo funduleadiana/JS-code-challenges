cc90d553f7872c7127adc3aa21ca181e6a4933f2
funduleadiana <diana.fundulea@yahoo.com>
2021-08-16 08:57:50 +0100

    steps solution 1

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,18 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for(let i =0; i<n; i++){
+    let stair = '';
+    for(let column = 0; column< n; column++){
+      if(column <= i){
+        stair += '#';
+      }else{
+        stair += ' ';
+      }
+    }
+    console.log(stair)
+  }
+}
 
 module.exports = steps;
