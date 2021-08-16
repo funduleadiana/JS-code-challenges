6eefae15adadde9031914f6ac96cd95efd3a9710
funduleadiana <diana.fundulea@yahoo.com>
2021-08-16 09:21:19 +0100

    recursion solution for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,18 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for(let i =0; i<n; i++){
-    let stair = '';
-    for(let column = 0; column< n; column++){
-      if(column <= i){
-        stair += '#';
-      }else{
-        stair += ' ';
-      }
-    }
-    console.log(stair)
+const { skipPartiallyEmittedExpressions } = require("typescript");
+
+
+//recursion solution
+function steps(n, row=0, stair='') {
+
+  if(n === row) return;
+
+  if(n === stair.length) {
+    console.log(stair);
+    return steps(n, row+1)
   }
+  if(stair.length <=row){
+    stair += '#';
+  }else{
+    stair += ' ';
+  }
+steps(n, row, stair)
+
+ 
 }
 
+
 module.exports = steps;
+
+
+
+// function steps(n) {
+//   for(let i =0; i<n; i++){
+//     let stair = '';
+//     for(let column = 0; column< n; column++){
+//       if(column <= i){
+//         stair += '#';
+//       }else{
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
