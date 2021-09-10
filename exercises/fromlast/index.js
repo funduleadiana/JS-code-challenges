03d9ae44eb9018eade4c2a774b766d1a6758253e
funduleadiana <diana.fundulea@yahoo.com>
2021-09-10 15:27:34 +0100

    finding a node inside a list n spaces from last

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -15,9 +15,22 @@ function fromLast(list, n) {
   //size - n => getAt(on the result )
       //if n = 0 => 0 spaces from the last element is the last element
       //slow/fast strategy 
+      let slow = list.head;
+      let fast = list.head;
+      while (n > 0){
+        fast = fast.next;
+        n--;
+      }
       //initialise as this.head; fast will move to the position indicated by the integer n 
       //is fast pointing at the last elemn in the list if no move forward
+      while(fast.next){
+        slow = slow.next;
+        fast = fast.next;
+
+      }
       //is it pointing at the last elemn if yes slow must be n elements behind it
+      return slow;
+      
 }
 
 module.exports = fromLast;
